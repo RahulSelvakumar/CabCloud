3cf675915b585d100fcf7943eaa0fa6e44cf2388
RahulS <rupash8754@gmail.com>
2024-04-23 13:15:45 +0530

    Final commit

--- src/CarListOptions.jsx
+++ src/CarListOptions.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
 import CarListData from "./CarListData.js";
 import CarListItem from "./CarListItem";
+import CabBooked from "./cab-booked";
 
 function CarListOptions({ distance }) {
   const [activeIndex, setActiveIndex] = useState();
+  const [selectedCar, setSelectedCar] = useState([]);
+
+  const handleOpenPaymentWindow = () => {
+     const newWindow = window.open("", "_blank", "width=600,height=400");
+       newWindow.document.write("<h1>Cab Booked Successfully!</h1>");
+  };
 
   return (
     <div className="mt-5 p-5 overflow-auto h-[350px]">
@@ -14,11 +21,25 @@ function CarListOptions({ distance }) {
           className={`cursor-pointer p-2 rounded-md border border-black ${
             activeIndex === index ? "border-[3px]" : ""
           }`}
-          onClick={() => setActiveIndex(index)}
+          onClick={() => {
+            setActiveIndex(index);
+            setSelectedCar(item);
+          }}
         >
           <CarListItem car={item} distance={distance} />
         </div>
       ))}
+      {selectedCar?.name && (
+        <div className="flex justify-between fixed bottom-5 bg-white p-3 rounded-lg shadow-xl w-full md:w-[30%] border-[1px] items-center">
+          <h2>Make Payment For</h2>
+          <button
+            className="p-3 bg-black text-white rounded-lg text-center"
+            onClick={handleOpenPaymentWindow}
+          >
+            Request {selectedCar.name}
+          </button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/cab-booked.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CabBookedPage = () => {
+  return (
+    <div>
+      <h2>Cab Booked!</h2>
+      <p>Your cab has been successfully booked.</p>
+    </div>
+  );
+};
+
+export default CabBookedPage;
